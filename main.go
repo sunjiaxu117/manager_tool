a7bd755ff08316ce238d900f38b239aec225228b
sunjiaxu117 <sunjiaxu@xiaomi.com>
2025-01-26 16:09:04 +0800

    feat:解决前端请求跨域

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"manger/log"
+	"time"
 
 	"manger/db"
 	"manger/routers"
@@ -13,6 +15,15 @@ func main() {
 	db.InitDB()
 	//服务器
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://127.0.0.1:5500"}, // 允许访问的前端域名，包括端口
+		AllowMethods:     []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	routers.RegisterRouters(server)
 	server.Run(":8888")
 
